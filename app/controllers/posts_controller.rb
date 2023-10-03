42dedc1b1ea395b1d153258b48da121a08575979
Dennis Lee <dennislee404@gmail.com>
2023-10-03 15:07:13 +0800

    done model post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,7 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:update, :edit, :show, :destroy]
 	def index
-		@posts = Post.all
-	end
-
-	def show
+		@posts = Post.all.order(created_at: :asc)
 	end
 
 	def new
@@ -11,14 +9,40 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		@post = Post.create(post_params)
+		if @post.save
+			redirect_to post_path(@post)
+		else
+			render :new
+		end
+	end
+
+	def show
 	end
 
 	def edit
 	end
 
 	def update
+		if @post.update(post_params)
+			redirect_to post_path(@post)
+		else
+			render :edit
+		end
 	end
 
 	def destroy
+		@post.destroy
+
+		redirect_to root_path
 	end
+
+	private
+		def set_post
+			@post = Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit( :caption, photos: [])
+		end
 end
