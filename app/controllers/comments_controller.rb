8e67dccb6e467a776e1d2ce058154b0f7fbcc732
Dennis Lee <dennislee404@gmail.com>
2023-10-05 10:50:22 +0800

    can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,14 @@ class CommentsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+
+		redirect_to post_path(@post)
+	end
+
 	private
 		def comment_params
 			params.require(:comment).permit(:content)
